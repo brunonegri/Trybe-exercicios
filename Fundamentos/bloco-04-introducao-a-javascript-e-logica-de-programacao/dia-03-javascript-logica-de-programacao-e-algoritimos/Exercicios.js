68f871fa6df191ff730e1f9d287cfcef5e577a60
Bruno Negri <b.negri_@hotmail.com>
2022-03-09 15:51:41 -0300

    Exercicio 1 concluido

---
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritimos/Exercicios.js
@@ -0,0 +1,11 @@
+// Exercicio 1
+
+// contagem = [1,2,3,4,5,6,7,8,9,10]
+// res = 1
+
+// for (i = 0; i < contagem.length; i++) {
+//     res *= contagem[i]
+//     console.log(res);   
+// }
+
+// Exercicio 2
