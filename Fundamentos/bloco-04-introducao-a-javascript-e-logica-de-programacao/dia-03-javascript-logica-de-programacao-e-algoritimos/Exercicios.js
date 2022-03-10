62b0193fcb79b67be781e33e0fc207676d4eab68
Bruno Negri <b.negri_@hotmail.com>
2022-03-10 13:50:39 -0300

    exercicios 4.3

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritimos/Exercicios.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritimos/Exercicios.js
@@ -5,7 +5,47 @@
 
 // for (i = 0; i < contagem.length; i++) {
 //     res *= contagem[i]
-//     console.log(res);   
 // }
+// console.log(res);
 
 // Exercicio 2
+
+// let word = 'tryber'
+// let reverse = ''
+// for (i = word.length -1; i >= 0; i--) {
+//     reverse += word[i]
+// }
+// console.log(reverse);
+
+// Exercicio 3
+
+// let array = ["java","javascript", "python", "html", "css"];
+
+// let menor = array[0].length;
+// let maior = array[0].length;
+
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i].length > maior) {
+//     maior = array[i];
+//     console.log(maior);
+//   }
+
+//   if (array[i].length < menor) {
+//     menor = array[i];
+//     console.log(menor);
+//   }
+// }
+
+// Exercicio 4
+
+for (let i = 2; i < 50; i++) {
+    let primo = true
+
+    for (let index = 2; index < i; index++) {
+        if (i % index === 0 ) {
+            naoPrimo = false;
+            break;
+        }
+    }
+    if (primo) console.log(i)
+}
---
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritimos/aulaAoVivo.js
@@ -0,0 +1,17 @@
+
+let soma = 0
+for (let index = 50; index < 100; index++) {
+        soma += index
+}
+console.log(soma);
+
+
+// let idades = [10 ,15 ,20]
+// let menor = 0
+
+// for (let index = 0; index < idades.length; index++) {
+//     if (idades[index]> menor) {
+//         menor = idades[index]
+//     }
+// }
+// console.log(idades[index])
